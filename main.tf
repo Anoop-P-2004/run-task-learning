6d2894c7776d22176ab3f0966d828cded66c2d6f
Anoop P <anoop171215@gmail.com>
2026-03-12 10:38:24 +0530

    Add Terraform configuration for local and random providers

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "local" {}
+
+provider "random" {}
+
+resource "random_pet" "name" {
+  length = 10
+}
+
+resource "local_file" "demo_file" {
+  content  = "Hello from Anoop P! Your random name is ${random_pet.name.id} \n I live in ${var.place}"
+  filename = "${path.module}/demo.txt"
+}
