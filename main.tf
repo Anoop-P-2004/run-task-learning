8a59860179d2390423fe1ab8e66528913da05d2c
Anoop P <anoop171215@gmail.com>
2026-04-13 12:17:22 +0530

    Update content of demo_file in main.tf
    Removed unnecessary text from the content of demo_file.

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "random_pet" "name" {
 }
 
 resource "local_file" "demo_file" {
-  content  = "Hello from Anoop! Your random name is ${random_pet.name.id} \n I live in ${var.place}. I am in main branch again...How are you?   "
+  content  = "Hello from Anoop! Your random name is ${random_pet.name.id} \n I live in ${var.place}. I am in main branch again... "
   filename = "${path.module}/demo.txt"
 }
